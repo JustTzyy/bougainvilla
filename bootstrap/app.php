50f533ae26a8676863d327ac2bf993c9c44ff3da
JustTzyy <digaljustin099@gmail.com>
2026-03-07 00:05:59 +0800

    Force HTTPS and trust proxies for Render deployment

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'rate.limit.login' => \App\Http\Middleware\RateLimitLogin::class,
         ]);
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
