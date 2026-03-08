e96e94fdfe390391889f6780c2e951ea27577116
JustTzyy <digaljustin099@gmail.com>
2026-03-08 16:17:32 +0800

    -final

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,8 +19,7 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             RoleSeeder::class,
-            UserSeeder::class,
-            BougainvillaHistoricalSeeder::class,
+         
         ]);
     }
 }
